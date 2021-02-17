251c92ce6e7ae2c8bde2a9190b2e76dc3dda6ba1
makotia <3mdevmail@gmail.com>
2021-02-17 12:49:04 +0900

    :+1: わからないけど作ってみた

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{ web, App, HttpResponse, HttpServer, Responder };
+use serde::{ Deserialize, Serialize };
+use rand::seq::SliceRandom;
+
+#[derive(Serialize, Deserialize)]
+struct DrogonObj {
+    text: String,
+}
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+async fn json_response() -> impl Responder {
+    let mut gen_dragon_vec = vec![
+        "ド",
+        "ラ",
+        "ゴ",
+        "ン",
+        "ボ",
+    ];
+    let mut dragon_vec = vec![];
+    for _ in 0..4 {
+        let mut rng = rand::thread_rng();
+        gen_dragon_vec.shuffle(&mut rng);
+        dragon_vec.push(gen_dragon_vec[0]);
+    }
+    HttpResponse::Ok().json(DrogonObj {
+        text: dragon_vec.join("").to_string(),
+    })
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/json", web::get().to(json_response))
+    })
+    .bind("127.0.0.1:8888")?
+    .run()
+    .await
 }
